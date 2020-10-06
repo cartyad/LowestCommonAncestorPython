a1879fbfbc77a6933e97fba3e0da440eb84954b7
cartyad <cartyad@tcd.ie>
2020-10-06 15:41:14 +0100

    Update Node.py
    Edits to the Tree inputted as a sample test

--- Node.py
+++ Node.py
@@ -1,31 +1,4 @@
 import math
-"""
-def findLCA(root, n1, n2):
-    path1 =[]
-    path2 =[]
-    if(not findPath(root,path1,n1) or not findPath(root,path2,n2)):
-        return -1
-    i=0
-    while(i< len(path1) and i<len(path2)):
-        if path1[i] != path2[i]:
-            break
-        i+=1
-    return path1[i-1]
-
-root = Node(1)
-root.left = Node(2)
-root.right = Node(3)   
-root.left.left = Node(4)
-root.left.right = Node(5)
-root.right.left = Node(6)
-root.right.right = Node(7)
-
-print("LCA(4, 5) = ", (findLCA(root, 4, 5))) 
-print("LCA(4, 6) = ", (findLCA(root, 4, 6)))
-print("LCA(3, 4) = ", (findLCA(root,3,4))) 
-print("LCA(2, 4) = ", (findLCA(root,2, 4))) 
-"""
-
 class Node: 
       
     
