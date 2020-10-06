7cfec656db7d0fb0b5564b8831312818dbf2c2f4
cartyad <cartyad@tcd.ie>
2020-10-06 12:16:09 +0100

    Update Node.py
    Saved before editing the node initialisation

--- Node.py
+++ Node.py
@@ -15,10 +15,10 @@ class Node:
             path.pop()
             return False
 
-    def findLca(root, n1, n2):
+    def findLCA(root, n1, n2):
         path1 =[]
         path2 =[]
-        if(not findpath(root,path1,n1) or not findPath(root,path2,n2)):
+        if(not findPath(root,path1,n1) or not findPath(root,path2,n2)):
             return -1
         i=0
         while(i< len(path1) and i<len(path2)):
@@ -26,7 +26,7 @@ class Node:
                 break
             i+=1
         return path1[i-1]
-    root =Node(1)
+    root = Node(1)
     root.left = Node(2)
     root.right = Node (3)   
     root.left.left = Node(4)
@@ -37,7 +37,7 @@ class Node:
     print("LCA(4, 5) = %d", (findLCA(root, 4, 5,))) 
     print("LCA(4, 6) = %d" %(findLCA(root, 4, 6)))
     print("LCA(3, 4) = %d" %(findLCA(root,3,4))) 
-    print "LCA(2, 4) = %d" %(findLCA(root,2, 4)) 
+    print("LCA(2, 4) = %d" %(findLCA(root,2, 4))) 
 
 
 
