5a508b36ba80fa7aabf5d6ce130be205dbfb74df
cartyad <cartyad@tcd.ie>
2020-10-06 12:11:38 +0100

    Update Node.py
    Corrected Print Statement

--- Node.py
+++ Node.py
@@ -35,7 +35,7 @@ class Node:
     root.right.right = Node(7)
 
     print("LCA(4, 5) = %d", (findLCA(root, 4, 5,))) 
-    print "LCA(4, 6) = %d" %(findLCA(root, 4, 6))
+    print("LCA(4, 6) = %d" %(findLCA(root, 4, 6)))
     print "LCA(3, 4) = %d" %(findLCA(root,3,4)) 
     print "LCA(2, 4) = %d" %(findLCA(root,2, 4)) 
 
