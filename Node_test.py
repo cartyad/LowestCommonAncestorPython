3178d26d2f6912fcb933669129c34bb4f84c1642
cartyad <cartyad@tcd.ie>
2020-10-18 19:43:43 +0100

    Removal of practice unit test
    Removed a practice unit test

--- Node_test.py
+++ Node_test.py
@@ -3,14 +3,6 @@ import Node
 
 class Node_test(unittest.TestCase):
     
-    def testFirst(self):
-        print("This is a test:")
-        result =True
-        self.assertEquals(result,True,"Oh no")
-        '''
-        root = Node.tree()
-        self.assertEqual(self.Node.findLCA(root, 4, 5),2)
-        ''' 
     def testRoot(self):
         root = Node.tree()
         self.assertEquals(root.key,1)
