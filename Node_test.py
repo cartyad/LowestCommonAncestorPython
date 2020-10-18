95237fa3a61bc9188158dbf7a1a173c6327b5b9a
cartyad <cartyad@tcd.ie>
2020-10-18 18:23:46 +0100

    Unit Testing
    Attempting to run unit testing

--- Node_test.py
+++ Node_test.py
@@ -1,18 +1,13 @@
 import unittest
-import Node
+'''import Node'''
 
 class Node_test(unittest.TestCase):
-    '''
+    
     def First_test(self):
+        print("This is a test:")
+        result =True
+        self.assertEquals(result,True,"Oh no")
+        '''
         root = self.Node.tree()
         self.assertEqual(self.Node.findLCA(root, 4, 5),2)
-    
-    def tree():
-        root = Node(1) 
-        root.left = Node(2) 
-        root.right = Node(3) 
-        root.left.left = Node(4) 
-        root.left.right = Node(5) 
-        root.right.left = Node(6) 
-        root.right.right = Node(7)
-'''    
+        '''    
